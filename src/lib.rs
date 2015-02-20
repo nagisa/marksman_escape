ab9127abd295fffcbc51248796ad2cff593cea39
Simonas Kazlauskas <git@kazlauskas.me>
2015-02-21 01:45:09 +0200

    Some optimisation

--- src/lib.rs
+++ src/lib.rs
@@ -2,34 +2,34 @@ use std::str::Bytes;
 use std::num::Int;
 
 const LONGEST_ESCAPE: usize = 6;
-static ZERO : [u8; LONGEST_ESCAPE + 1] = [0; LONGEST_ESCAPE + 1];
+static ZERO : [u8; LONGEST_ESCAPE] = [0; LONGEST_ESCAPE];
 // Basic set of escapes: &, <, >, "
-static AMP  : [u8; LONGEST_ESCAPE + 1] = [0x61, 0x6d, 0x70, 0x3b, 0, 0, 0];
-static LT   : [u8; LONGEST_ESCAPE + 1] = [0x6c, 0x74, 0x3b, 0, 0, 0, 0];
-static GT   : [u8; LONGEST_ESCAPE + 1] = [0x67, 0x74, 0x3b, 0, 0, 0, 0];
-static QUOT : [u8; LONGEST_ESCAPE + 1] = [0x23, 0x33, 0x34, 0x3b, 0, 0, 0];
+static AMP  : [u8; LONGEST_ESCAPE] = [0x61, 0x6d, 0x70, 0x3b, 0, 0];
+static LT   : [u8; LONGEST_ESCAPE] = [0x6c, 0x74, 0x3b, 0, 0, 0];
+static GT   : [u8; LONGEST_ESCAPE] = [0x67, 0x74, 0x3b, 0, 0, 0];
+static QUOT : [u8; LONGEST_ESCAPE] = [0x23, 0x33, 0x34, 0x3b, 0, 0];
 // Less basic set, but important nevertheless: ' and `
 // Grave is supposedly allowed in IE as attribute value wrapper.
-static APOS : [u8; LONGEST_ESCAPE + 1] = [0x23, 0x33, 0x39, 0x3b, 0, 0, 0];
-static GRV  : [u8; LONGEST_ESCAPE + 1] = [0x23, 0x39, 0x36, 0x3b, 0, 0, 0];
+static APOS : [u8; LONGEST_ESCAPE] = [0x23, 0x33, 0x39, 0x3b, 0, 0];
+static GRV  : [u8; LONGEST_ESCAPE] = [0x23, 0x39, 0x36, 0x3b, 0, 0];
 // Now these only matter in cases where attributes are not quoted.
-static BANG : [u8; LONGEST_ESCAPE + 1] = [0x23, 0x33, 0x33, 0x3b, 0, 0, 0];
-static USD  : [u8; LONGEST_ESCAPE + 1] = [0x23, 0x33, 0x36, 0x3b, 0, 0, 0];
-static PERC : [u8; LONGEST_ESCAPE + 1] = [0x23, 0x33, 0x37, 0x3b, 0, 0, 0];
-static LPR  : [u8; LONGEST_ESCAPE + 1] = [0x23, 0x34, 0x30, 0x3b, 0, 0, 0];
-static RPR  : [u8; LONGEST_ESCAPE + 1] = [0x23, 0x34, 0x31, 0x3b, 0, 0, 0];
-static PLUS : [u8; LONGEST_ESCAPE + 1] = [0x23, 0x34, 0x33, 0x3b, 0, 0, 0];
-static EQ   : [u8; LONGEST_ESCAPE + 1] = [0x23, 0x36, 0x31, 0x3b, 0, 0, 0];
-static AT   : [u8; LONGEST_ESCAPE + 1] = [0x23, 0x36, 0x34, 0x3b, 0, 0, 0];
-static LBR  : [u8; LONGEST_ESCAPE + 1] = [0x23, 0x39, 0x31, 0x3b, 0, 0, 0];
-static RBR  : [u8; LONGEST_ESCAPE + 1] = [0x23, 0x39, 0x33, 0x3b, 0, 0, 0];
-static LBRK : [u8; LONGEST_ESCAPE + 1] = [0x23, 0x31, 0x32, 0x33, 0x3b, 0, 0];
-static RBRK : [u8; LONGEST_ESCAPE + 1] = [0x23, 0x31, 0x32, 0x35, 0x3b, 0, 0];
+static BANG : [u8; LONGEST_ESCAPE] = [0x23, 0x33, 0x33, 0x3b, 0, 0];
+static USD  : [u8; LONGEST_ESCAPE] = [0x23, 0x33, 0x36, 0x3b, 0, 0];
+static PERC : [u8; LONGEST_ESCAPE] = [0x23, 0x33, 0x37, 0x3b, 0, 0];
+static LPR  : [u8; LONGEST_ESCAPE] = [0x23, 0x34, 0x30, 0x3b, 0, 0];
+static RPR  : [u8; LONGEST_ESCAPE] = [0x23, 0x34, 0x31, 0x3b, 0, 0];
+static PLUS : [u8; LONGEST_ESCAPE] = [0x23, 0x34, 0x33, 0x3b, 0, 0];
+static EQ   : [u8; LONGEST_ESCAPE] = [0x23, 0x36, 0x31, 0x3b, 0, 0];
+static AT   : [u8; LONGEST_ESCAPE] = [0x23, 0x36, 0x34, 0x3b, 0, 0];
+static LBR  : [u8; LONGEST_ESCAPE] = [0x23, 0x39, 0x31, 0x3b, 0, 0];
+static RBR  : [u8; LONGEST_ESCAPE] = [0x23, 0x39, 0x33, 0x3b, 0, 0];
+static LBRK : [u8; LONGEST_ESCAPE] = [0x23, 0x31, 0x32, 0x33, 0x3b, 0];
+static RBRK : [u8; LONGEST_ESCAPE] = [0x23, 0x31, 0x32, 0x35, 0x3b, 0];
 
 pub struct Escaped<'a> {
     inner: Bytes<'a>,
     idx: usize,
-    inner_buffer: &'static [u8; LONGEST_ESCAPE + 1]
+    inner_buffer: &'static [u8; LONGEST_ESCAPE]
 }
 
 impl<'a> Escaped<'a> {
@@ -42,7 +42,7 @@ impl<'a> Escaped<'a> {
     }
 
     #[inline(always)]
-    fn buf(&mut self, escape: &'static [u8; LONGEST_ESCAPE + 1]) -> Option<u8> {
+    fn buf(&mut self, escape: &'static [u8; LONGEST_ESCAPE]) -> Option<u8> {
         self.idx = 0;
         self.inner_buffer = escape;
         Some(0x26)
@@ -53,10 +53,12 @@ impl<'a> Escaped<'a> {
 impl<'a> Iterator for Escaped<'a> {
     type Item = u8;
     fn next(&mut self) -> Option<<Self as Iterator>::Item> {
-        if self.inner_buffer[self.idx] != 0 {
-            let r = self.inner_buffer[self.idx];
-            self.idx += 1;
-            return Some(r);
+        unsafe {
+            if *self.inner_buffer.get_unchecked(self.idx) != 0 {
+                let r = *self.inner_buffer.get_unchecked(self.idx);
+                self.idx += 1;
+                return Some(r);
+            }
         }
         if let Some(ch) = self.inner.next() {
             match ch {
