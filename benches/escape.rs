35ec00e4b1ef8d304981ef4ef9f4d9adb30ec628
Simonas Kazlauskas <git@kazlauskas.me>
2015-02-22 14:49:26 +0200

    Clean up benchmarks

---
+++ benches/escape.rs
@@ -0,0 +1,104 @@
+#![feature(test)]
+extern crate test;
+extern crate marksman_escape;
+
+use test::Bencher;
+use test::black_box;
+use marksman_escape::Escape;
+use std::str::StrExt;
+
+
+static MX_ESCAPES : &'static str = "a<cd>f{bcd}f<bcd>f{bc}e'ab\"d?f@b!d`f{bcd}f&b=d+f(bcd)f%bc$e!
+a<cd>f{bcd}f<bcd>f{bc}e'ab\"d?f@b!d`f{bcd}f&b=d+f(bcd)f%bc$e!s[ytr]02\u{A0}a<cd>f{bcd}f&^<>@!+=@
+a<cd>f{bcd}f<bcd>f{bc}e'ab\"d?f@b!d`f{bcd}f&b=d+f(bcd)f%bc$e!s[ytr]02\u{A0}a<cd>f{bcd}f&^<>@!+=@
+a<cd>f{bcd}f<bcd>f{bc}e'ab\"d?f@b!d`f{bcd}f&b=d+f(bcd)f%bc$e!s[ytr]02\u{A0}a<cd>f{bcd}f&^<>@!+=@
+a<cd>f{bcd}f<bcd>f{bc}e'ab\"d?f@b!d`f{bcd}f&b=d+f(bcd)f%bc$e!s[ytr]02\u{A0}a<cd>f{bcd}f&^<>@!+=@
+a<cd>f{bcd}f<bcd>f{bc}e'ab\"d?f@b!d`f{bcd}f&b=d+f(bcd)f%bc$e!s[ytr]02\u{A0}a<cd>f{bcd}f&^<>@!+=@
+a<cd>f{bcd}f<bcd>f{bc}e'ab\"d?f@b!d`f{bcd}f&b=d+f(bcd)f%bc$e!s[ytr]02\u{A0}a<cd>f{bcd}f&^<>@!+=@
+a<cd>f{bcd}f<bcd>f{bc}e'ab\"d?f@b!d`f{bcd}f&b=d+f(bcd)f%bc$e!s[ytr]02\u{A0}a<cd>f{bcd}f&^<>@!+=@
+a<cd>f{bcd}f<bcd>f{bc}e'ab\"d?f@b!d`f{bcd}f&b=d+f(bcd)f%bc$e!s[ytr]02\u{A0}a<cd>f{bcd}f&^<>@!+=@
+a<cd>f{bcd}f<bcd>f{bc}e'ab\"d?f@b!d`f{bcd}f&b=d+f(bcd)f%bc$e!s[ytr]02\u{A0}a<cd>f{bcd}f&^<>@!+=@
+a<cd>f{bcd}f<bcd>f{bc}e'ab\"d?f@b!d`f{bcd}f&b=d+f(bcd)f%bc$e!s[ytr]02\u{A0}a<cd>f{bcd}f&^<>@!+=@";
+
+#[bench]
+fn escape_mixed(b: &mut Bencher){
+    b.bytes = MX_ESCAPES.len() as u64;
+    b.iter(||{
+        for _ in Escape::new(MX_ESCAPES.bytes()) {
+            black_box(());
+        }
+    });
+}
+
+
+static NO_ESCAPES : &'static str = r##"abcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdef
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabc"##;
+
+#[bench]
+fn escape_no_spec(b: &mut Bencher){
+    b.bytes = NO_ESCAPES.len() as u64;
+    b.iter(||{
+        for _ in Escape::new(NO_ESCAPES.bytes()) {
+            black_box(());
+        }
+    });
+}
+
+
+static ALL_ESCAPES_SHORT : &'static str = r##"<><><><><><><><><><><><><><><><><><><><><><><><><><><
+><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>"##;
+
+#[bench]
+fn escape_spec_short(b: &mut Bencher){
+    b.bytes = ALL_ESCAPES_SHORT.len() as u64;
+    b.iter(||{
+        for _ in Escape::new(ALL_ESCAPES_SHORT.bytes()) {
+            black_box(());
+        }
+    });
+}
+
+
+static ALL_ESCAPES_LONG : &'static str = "[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}
+[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]
+[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]
+[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]
+[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]
+[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]
+[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]{}[]";
+
+#[bench]
+fn escape_spec_long(b: &mut Bencher){
+    b.bytes = ALL_ESCAPES_LONG.len() as u64;
+    b.iter(||{
+        for _ in Escape::new(ALL_ESCAPES_LONG.bytes()) {
+            black_box(());
+        }
+    });
+}
---
+++ benches/unescape.rs
@@ -0,0 +1,140 @@
+#![feature(test)]
+extern crate test;
+extern crate marksman_escape;
+
+use test::Bencher;
+use test::black_box;
+use marksman_escape::{Unescape, get_named_ref};
+use std::str::StrExt;
+
+static NO_ESCAPES : &'static str = r##"abcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdef
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabcdefa
+fabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdeffabcdefabcdefabcdefabcdefabcdefabcdefabc"##;
+
+#[bench]
+fn unescape_no_spec(b: &mut Bencher){
+    b.bytes = NO_ESCAPES.len() as u64;
+    b.iter(||{
+        for _ in Unescape::new(NO_ESCAPES.bytes()) {
+            black_box(());
+        }
+    });
+}
+
+
+
+static ALL_ESCAPES_NAMED : &'static str = r##"&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp
+&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp
+&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp
+&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp
+&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp
+&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp
+&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp
+&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp
+&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp
+&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp&lt;&gt;&amp;&lt&gt&amp"##;
+
+#[bench]
+fn unescape_spec_named(b: &mut Bencher){
+    b.bytes = ALL_ESCAPES_NAMED.len() as u64;
+    b.iter(||{
+        for _ in Unescape::new(ALL_ESCAPES_NAMED.bytes()) {
+            black_box(());
+        }
+    });
+}
+
+
+
+static ALL_ESCAPES_NUM : &'static str = r##"&#91;&#93;&#123;&#125;&#91;&#93;&#123;&#125;&#91;&#93;
+&#91;&#93;&#123;&#125;&#91;&#93;&#123;&#125;&#91;&#93;&#91;&#93;&#123;&#125;&#91;&#93;&#123;&#125;
+&#91;&#93;&#123;&#125;&#91;&#93;&#123;&#125;&#91;&#93;&#91;&#93;&#123;&#125;&#91;&#93;&#123;&#125;
+&#91;&#93;&#123;&#125;&#91;&#93;&#123;&#125;&#91;&#93;&#91;&#93;&#123;&#125;&#91;&#93;&#123;&#125;
+&#91;&#93;&#123;&#125;&#91;&#93;&#123;&#125;&#91;&#93;&#91;&#93;&#123;&#125;&#91;&#93;&#123;&#125;
+&#91;&#93;&#123;&#125;&#91;&#93;&#123;&#125;&#91;&#93;&#91;&#93;&#123;&#125;&#91;&#93;&#123;&#125;
+&#91;&#93;&#123;&#125;&#91;&#93;&#123;&#125;&#91;&#93;&#91;&#93;&#123;&#125;&#91;&#93;&#123;&#125;
+&#91;&#93;&#123;&#125;&#91;&#93;&#123;&#125;&#91;&#93;&#91;&#93;&#123;&#125;&#91;&#93;&#123;&#125;
+&#91;&#93;&#123;&#125;&#91;&#93;&#123;&#125;&#91;&#93;&#91;&#93;&#123;&#125;&#91;&#93;&#123;"##;
+
+#[bench]
+fn unescape_spec_num(b: &mut Bencher){
+    b.bytes = ALL_ESCAPES_NUM.len() as u64;
+    b.iter(||{
+        for _ in Unescape::new(ALL_ESCAPES_NUM.bytes()) {
+            black_box(());
+        }
+    });
+}
+
+static ALL_ESCAPES_HEX : &'static str = r##"&#x5B;&#x5d;&#x7B;&#7d;&#x5B;&#x5d;&#x7B;&#7d;&#x5B;
+&#x5B;&#x5d;&#x7B;&#7d;&#x5B;&#x5d;&#x7B;&#7d;&#x5B;&#x5B;&#x5d;&#x7B;&#7d;&#x5B;&#x5d;&#x7B;&#7d;
+&#x5B;&#x5d;&#x7B;&#7d;&#x5B;&#x5d;&#x7B;&#7d;&#x5B;&#x5B;&#x5d;&#x7B;&#7d;&#x5B;&#x5d;&#x7B;&#7d;
+&#x5B;&#x5d;&#x7B;&#7d;&#x5B;&#x5d;&#x7B;&#7d;&#x5B;&#x5B;&#x5d;&#x7B;&#7d;&#x5B;&#x5d;&#x7B;&#7d;
+&#x5B;&#x5d;&#x7B;&#7d;&#x5B;&#x5d;&#x7B;&#7d;&#x5B;&#x5B;&#x5d;&#x7B;&#7d;&#x5B;&#x5d;&#x7B;&#7d;
+&#x5B;&#x5d;&#x7B;&#7d;&#x5B;&#x5d;&#x7B;&#7d;&#x5B;&#x5B;&#x5d;&#x7B;&#7d;&#x5B;&#x5d;&#x7B;&#7d;
+&#x5B;&#x5d;&#x7B;&#7d;&#x5B;&#x5d;&#x7B;&#7d;&#x5B;&#x5B;&#x5d;&#x7B;&#7d;&#x5B;&#x5d;&#x7B;&#7d;
+&#x5B;&#x5d;&#x7B;&#7d;&#x5B;&#x5d;&#x7B;&#7d;&#x5B;&#x5B;&#x5d;&#x7B;&#7d;&#x5B;&#x5d;&#x7B;&#7d;
+&#x5B;&#x5d;&#x7B;&#7d;&#x5B;&#x5d;&#x7B;&#7d;&#x5B;&#x5B;&#x5d;&#x7B;&#7d;&#x5B;&#x5d;&#x7B;"##;
+
+#[bench]
+fn unescape_spec_hex(b: &mut Bencher){
+    b.bytes = ALL_ESCAPES_HEX.len() as u64;
+    b.iter(||{
+        for _ in Unescape::new(ALL_ESCAPES_HEX.bytes()) {
+            black_box(());
+        }
+    });
+}
+
+
+
+#[bench]
+fn named_ref_various(b: &mut Bencher){
+    b.iter(||{
+        black_box(get_named_ref(b"amp;").unwrap());
+        black_box(get_named_ref(b"lt;").unwrap());
+        black_box(get_named_ref(b"gt;").unwrap());
+        black_box(get_named_ref(b"zwj;").unwrap());
+        black_box(get_named_ref(b"ZeroWidthSpace;").unwrap());
+        black_box(get_named_ref(b"Yacute;").unwrap());
+    });
+}
+
+#[bench]
+fn named_ref_simple_common(b: &mut Bencher){
+    b.iter(||{
+        black_box(get_named_ref(b"amp;").unwrap());
+        black_box(get_named_ref(b"lt;").unwrap());
+        // black_box(get_named_ref(b"gt;").unwrap());
+        black_box(get_named_ref(b"amp;").unwrap());
+        black_box(get_named_ref(b"lt;").unwrap());
+        // black_box(get_named_ref(b"gt;").unwrap());
+    });
+}
+
+#[bench]
+fn named_ref_rare(b: &mut Bencher){
+    b.iter(||{
+        black_box(get_named_ref(b"zwj;").unwrap());
+        black_box(get_named_ref(b"ZeroWidthSpace;").unwrap());
+        black_box(get_named_ref(b"Yacute;").unwrap());
+        black_box(get_named_ref(b"zwj;").unwrap());
+        black_box(get_named_ref(b"ZeroWidthSpace;").unwrap());
+        black_box(get_named_ref(b"Yacute;").unwrap());
+    });
+}
